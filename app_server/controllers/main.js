5d4dc5dbd2e002c809290c92800a4f4f0f000e72
saitejabollikonda <saitejabollikonda2023@gmail.com>
2024-09-05 01:32:31 -0700

    Delete app_server directory

--- app_server/controllers/main.js
+++
@@ -1,7 +0,0 @@
-/* GET homepage */
-const index = (req, res) => {
-    res.render('index', { title: 'Class Project' });
-    };
-module.exports = {
-    index
-    };
--- app_server/controllers/others.js
+++
@@ -1,7 +0,0 @@
-/* GET 'about' page */
-const about = (req, res) => {
-    res.render('generic-text', { title: 'About' });
-    };
-    module.exports = {
-    about
-    };
